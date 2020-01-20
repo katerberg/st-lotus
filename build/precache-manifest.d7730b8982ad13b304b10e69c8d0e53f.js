7dd44055c03a061aa47b83e0ede708293a9c5b23
Mark Katerberg <katerberg@fastmail.fm>
2020-01-19 20:24:25 -0600

    built artifacts

---
+++ build/precache-manifest.d7730b8982ad13b304b10e69c8d0e53f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf7472f6d3853b506a447dcf8f3d21b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c62df1a6d129334516f4",
+    "url": "./static/css/main.6a54d1c1.chunk.css"
+  },
+  {
+    "revision": "ccbe4827a20aee3b5050",
+    "url": "./static/js/2.e65cee22.chunk.js"
+  },
+  {
+    "revision": "c62df1a6d129334516f4",
+    "url": "./static/js/main.8eae610b.chunk.js"
+  },
+  {
+    "revision": "af009904b7d97c7ffcc9",
+    "url": "./static/js/runtime-main.9a6889a6.js"
+  }
+]);
