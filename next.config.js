c363ec5d7a44d08b8a1c35f43a8aef3fdd47a80d
katerberg <katerberg@fastmail.fm>
2023-11-07 04:36:44 +0000

    Deploying to public from @ katerberg/st-lotus@6578e69e758978455ed0a94023d1ae5479d7834f 🚀

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: 'export'
-};
-
-module.exports = nextConfig;
--- src/app/react-app-env.d.ts
+++
@@ -1,5 +0,0 @@
-interface Window {
-    ga: (command: string, typeModifier: string, path?: string) => void;
-    addEventListener: (name: string, callback: () => void) => void;
-    removeEventListener: (name: string, callback: () => void) => void;
-}
