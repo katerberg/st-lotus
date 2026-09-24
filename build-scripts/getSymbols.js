5b15b1ca921d4b5c302f0efc4a5ca29408f63dcd
Mark Katerberg <katerberg@fastmail.fm>
2026-09-24 14:45:56 -0500

    Fix mana symbols

--- build-scripts/getSymbols.js
+++ build-scripts/getSymbols.js
@@ -1,20 +1,27 @@
 /* eslint-disable no-console */
-const fs = require('fs');
-const util = require('util');
-const axios = require('axios');
+const fs = require("fs");
+const util = require("util");
+const axios = require("axios");
 
 fs.readFileAsync = util.promisify(fs.readFile);
 fs.writeFileAsync = util.promisify(fs.writeFile);
 
 async function getSymbols() {
-  const axiosCall = await axios.get('https://api.scryfall.com/symbology');
-  return axiosCall.data.data.reduce((a, c) => ({...a, [c.symbol]: c.svg_uri}), {});
+  const axiosCall = await axios.get("https://api.scryfall.com/symbology");
+  return axiosCall.data.data.reduce(
+    (a, c) => ({ ...a, [c.symbol]: c.svg_uri }),
+    {},
+  );
 }
 
-getSymbols().then(sortedSymbols => {
-  fs.writeFile(`${process.cwd()}/src/decks/symbols.json`, JSON.stringify(sortedSymbols, null, 2), (err) => {
-    if (err) {
-      throw err;
-    }
-  });
+getSymbols().then((sortedSymbols) => {
+  fs.writeFile(
+    `${process.cwd()}/src/common/mana-cost/symbols.json`,
+    JSON.stringify(sortedSymbols, null, 2),
+    (err) => {
+      if (err) {
+        throw err;
+      }
+    },
+  );
 });
